aa3d96a12cb46e02549ddc8a553f5ad6a3adc445
Cinorid <cinorid@gmail.com>
2021-12-06 12:46:58 +0330

    fix no qr showing

--- Google Authenticator Backup Tool/MainWindow.xaml.cs
+++ Google Authenticator Backup Tool/MainWindow.xaml.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SQLite;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -14,22 +14,28 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ZXing;
+using ZXing.QrCode;
+using ZXing.QrCode.Internal;
+using Color = System.Windows.Media.Color;
+using Image = System.Windows.Controls.Image;
 
 namespace Google_Authenticator_Backup_Tool
 {
+	public class GAItem
+	{
+		public string email { set; get; }
+		public string issuer { set; get; }
+		public string secret { set; get; }
+		public string url { set; get; }
+		public ImageSource image { set; get; }
+	}
+	
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
 	public partial class MainWindow : Window
 	{
-		public class GAItem
-		{
-			public string email { set; get; }
-			public string issuer { set; get; }
-			public string secret { set; get; }
-			public string url { set; get; }
-		}
-
 		List<GAItem> items = new List<GAItem>();
 
 		public MainWindow()
@@ -104,6 +110,14 @@ namespace Google_Authenticator_Backup_Tool
 							items.Add(new GAItem() { email = item.GetValue(0).ToString(), secret = item.GetValue(1).ToString(), issuer = item.GetValue(2).ToString() });
 						}
 
+						var qrWriter = new BarcodeWriter();
+						qrWriter.Format = BarcodeFormat.QR_CODE;
+						qrWriter.Options.Hints.Add(EncodeHintType.CHARACTER_SET, "UTF-8");
+						qrWriter.Options.Hints.Add(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+						qrWriter.Options.Hints.Add(EncodeHintType.MARGIN, 1);
+						qrWriter.Options.Hints[EncodeHintType.WIDTH] = 250;
+						qrWriter.Options.Hints[EncodeHintType.HEIGHT] = 250;
+						
 						foreach(var item in items)
 						{
 							if (string.IsNullOrEmpty(item.issuer))
@@ -125,6 +139,9 @@ namespace Google_Authenticator_Backup_Tool
 
 							var url = string.Format("otpauth://totp/{0}?secret={1}&issuer={2}", item.email, item.secret, item.issuer);
 							item.url = url;
+							
+							var bmp = qrWriter.Write(item.url);
+							item.image = BitmapToImageSource(bmp);
 						}
 					}
 					
@@ -132,39 +149,21 @@ namespace Google_Authenticator_Backup_Tool
 			}
 
 			lstResults.ItemsSource = items;
-
-			/*
-			mkdir authenticator-backup
-			cd authenticator-backup
-			adb root
-			adb pull /data/data/com.google.android.apps.authenticator2/databases/databases
-			sqlite3 ./databases "select * from accounts"
-
-
-			import qrcode
-			import sqlite3
-			conn = sqlite3.connect('./databases')
-			c = conn.cursor()
-
-			for idx, (email, secret, issuer) in enumerate(c.execute("SELECT email,secret,issuer FROM accounts").fetchall()):
-			   if issuer==None:
-				   if len(email.split(" "))>0:
-					   issuer=email.split(" ")[0]
-				   else:
-					   issuer=email
-
-				   if len(issuer.split(":"))>0:
-					   issuer=issuer.split(":")[0]
-
-				   print("If the following issuer looks wrong, enter a new value. If it's OK, just press ENTER.")
-				   newIssuer=input(issuer)
-				   if len(newIssuer)>0:
-					   issuer=newIssuer
-			   url = 'otpauth://totp/{}?secret={}&issuer={}'.format(email, secret, issuer)
-			   print (url)
-			   im = qrcode.make(url)
-			   im.save('./{}.png'.format(idx))
-			*/
+		}
+		
+		private BitmapImage BitmapToImageSource(System.Drawing.Bitmap bitmap)
+		{
+			using (MemoryStream memory = new MemoryStream())
+			{
+				bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+				memory.Position = 0;
+				BitmapImage bitmapimage = new BitmapImage();
+				bitmapimage.BeginInit();
+				bitmapimage.StreamSource = memory;
+				bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+				bitmapimage.EndInit();
+				return bitmapimage;
+			}
 		}
 	}
 }
