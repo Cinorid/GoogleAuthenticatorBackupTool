b27fe6e4504d47869b73dfe8352eb860365e0d5f
Cinorid <cinorid@gmail.com>
2022-06-11 00:25:27 +0430

    add export and open db button

--- Google Authenticator Backup Tool/MainWindow.xaml.cs
+++ Google Authenticator Backup Tool/MainWindow.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using Newtonsoft.Json;
 using ZXing;
 using ZXing.QrCode;
 using ZXing.QrCode.Internal;
@@ -27,10 +29,19 @@ namespace Google_Authenticator_Backup_Tool
 		public string email { set; get; }
 		public string issuer { set; get; }
 		public string secret { set; get; }
-		public string url { set; get; }
-		public ImageSource image { set; get; }
+		[JsonIgnore] public string url { set; get; }
+		[JsonIgnore] public ImageSource image { set; get; }
+		public string label => email;
+		public int digits => 6;
+		public string type => "TOTP";
+		public string algorithm => "SHA1";
+		public string thumbnail => "Default";
+		public long last_used => 0;
+		public int used_frequency => 0;
+		public int period => 30;
+		public string[] tags => new string[] { };
 	}
-	
+
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
@@ -48,7 +59,7 @@ namespace Google_Authenticator_Backup_Tool
 			
 		}
 
-		private void Button_Click(object sender, RoutedEventArgs e)
+		private void ButtonStart_Click(object sender, RoutedEventArgs e)
 		{
 			lstOperations.Items.Clear();
 			items.Clear();
@@ -97,58 +108,10 @@ namespace Google_Authenticator_Backup_Tool
 					sw.WriteLine("sqlite3 ./databases \"select * from accounts\"");
 					System.Threading.Thread.Sleep(100);
 
-					using (SQLiteConnection connection = new SQLiteConnection("Data Source="+ AppDomain.CurrentDomain.BaseDirectory + "\\authenticator-backup\\databases;Version=3;"))
-					{
-						connection.Open();
-
-						var cmd = connection.CreateCommand();
-						cmd.CommandText = "SELECT email,secret,issuer FROM accounts";
-						var reader = cmd.ExecuteReader();
-
-						foreach(System.Data.Common.DbDataRecord item in reader)
-						{
-							items.Add(new GAItem() { email = item.GetValue(0).ToString(), secret = item.GetValue(1).ToString(), issuer = item.GetValue(2).ToString() });
-						}
-
-						var qrWriter = new BarcodeWriter();
-						qrWriter.Format = BarcodeFormat.QR_CODE;
-						qrWriter.Options.Hints.Add(EncodeHintType.CHARACTER_SET, "UTF-8");
-						qrWriter.Options.Hints.Add(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
-						qrWriter.Options.Hints.Add(EncodeHintType.MARGIN, 1);
-						qrWriter.Options.Hints[EncodeHintType.WIDTH] = 250;
-						qrWriter.Options.Hints[EncodeHintType.HEIGHT] = 250;
-						
-						foreach(var item in items)
-						{
-							if (string.IsNullOrEmpty(item.issuer))
-							{
-								if (item.email.Split(' ').Length > 0)
-								{
-									item.issuer = item.email.Split(' ')[0];
-								}
-								else
-								{
-									item.issuer = item.email;
-								}
-
-								if (item.issuer.Split(':').Length > 0)
-								{
-									item.issuer = item.issuer.Split(':')[0];
-								}
-							}
-
-							var url = string.Format("otpauth://totp/{0}?secret={1}&issuer={2}", item.email, item.secret, item.issuer);
-							item.url = url;
-							
-							var bmp = qrWriter.Write(item.url);
-							item.image = BitmapToImageSource(bmp);
-						}
-					}
-					
+					string filePath = AppDomain.CurrentDomain.BaseDirectory + "\\authenticator-backup\\databases";
+					ExtractDB(filePath);
 				}
 			}
-
-			lstResults.ItemsSource = items;
 		}
 		
 		private BitmapImage BitmapToImageSource(System.Drawing.Bitmap bitmap)
@@ -164,6 +127,92 @@ namespace Google_Authenticator_Backup_Tool
 				bitmapimage.EndInit();
 				return bitmapimage;
 			}
+		}
+
+		private void ButtonExport_Click(object sender, RoutedEventArgs e)
+		{
+			var sfd = new SaveFileDialog();
+			sfd.Filter = "JSON File|*.json";
+			sfd.Title = "Export Google Authenticator Backup";
+			sfd.FileName = $"otp_accounts_{DateTime.Now.ToString("yyyyMMddHHmmss")}.json";
+			if(sfd.ShowDialog() == true)
+			{
+				var json = JsonConvert.SerializeObject(items.ToArray(), Formatting.Indented);
+				File.WriteAllText(sfd.FileName, json);
+			}
+		}
+
+		private void ButtonOpenDB_Click(object sender, RoutedEventArgs e)
+		{
+			var ofd = new OpenFileDialog();
+			ofd.Filter = "SQLite File|*.*";
+			ofd.Title = "Open Google Authenticator DB";
+			if(ofd.ShowDialog() == true)
+			{
+				string filePath = ofd.FileName;
+				ExtractDB(filePath);
+			}
+		}
+
+		private void ExtractDB(string dbPath)
+		{
+			lstOperations.Items.Clear();
+			items.Clear();
+
+			using (SQLiteConnection connection = new SQLiteConnection("Data Source="+ dbPath + ";Version=3;"))
+			{
+				connection.Open();
+
+				var cmd = connection.CreateCommand();
+				cmd.CommandText = "SELECT email,secret,issuer FROM accounts";
+				var reader = cmd.ExecuteReader();
+
+				foreach(System.Data.Common.DbDataRecord item in reader)
+				{
+					items.Add(new GAItem()
+					{
+						email = item.GetValue(0).ToString(),
+						secret = item.GetValue(1).ToString(),
+						issuer = item.GetValue(2).ToString()
+					});
+				}
+
+				var qrWriter = new BarcodeWriter();
+				qrWriter.Format = BarcodeFormat.QR_CODE;
+				qrWriter.Options.Hints.Add(EncodeHintType.CHARACTER_SET, "UTF-8");
+				qrWriter.Options.Hints.Add(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+				qrWriter.Options.Hints.Add(EncodeHintType.MARGIN, 1);
+				qrWriter.Options.Hints[EncodeHintType.WIDTH] = 250;
+				qrWriter.Options.Hints[EncodeHintType.HEIGHT] = 250;
+						
+				foreach(var item in items)
+				{
+					if (string.IsNullOrEmpty(item.issuer))
+					{
+						if (item.email.Split(' ').Length > 0)
+						{
+							item.issuer = item.email.Split(' ')[0];
+						}
+						else
+						{
+							item.issuer = item.email;
+						}
+
+						if (item.issuer.Split(':').Length > 0)
+						{
+							item.issuer = item.issuer.Split(':')[0];
+						}
+					}
+
+					var url = string.Format("otpauth://totp/{0}?secret={1}&issuer={2}", item.email, item.secret, item.issuer);
+					item.url = url;
+							
+					var bmp = qrWriter.Write(item.url);
+					item.image = BitmapToImageSource(bmp);
+				}
+			}
+
+			lstResults.ItemsSource = items;
 		}
 	}
 }
